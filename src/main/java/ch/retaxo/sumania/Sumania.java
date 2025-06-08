b7dcb94bfbb91f0ed77acbf85926a9849b61652c
Retaxo <105597296+sudo-Tiger@users.noreply.github.com>
2025-06-08 22:15:28 +0200

    First Commit

---
+++ src/main/java/ch/retaxo/sumania/Sumania.java
@@ -0,0 +1,17 @@
+package ch.retaxo.sumania;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sumania extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
